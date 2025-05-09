3e45c4fa61b727184d4708e24061fd916ef8d322
Barbie-Beauty <diego_gng1400@hotmail.com>
2025-05-09 11:46:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 async function cotizar() {
   const input = document.getElementById("imagen");
   const resultado = document.getElementById("resultado");
@@ -19,11 +18,9 @@ async function cotizar() {
 
     try {
       const res = await fetch("https://barbie-beauty-backend.onrender.com/analizar", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ imagen: imagenBase64, token: "barbie1234" })
-});
-        
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ imagen: imagenBase64, token: "barbie1234" })
       });
 
       const data = await res.json();
